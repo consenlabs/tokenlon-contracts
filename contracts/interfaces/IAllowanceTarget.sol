520d25e771d8ea927cf713c72c3d941f9483a216
Charles Jhong <charlesjhongc@gmail.com>
2022-05-06 01:25:01 +0800

    add Spender and AllowanceTarget

---
+++ contracts/interfaces/IAllowanceTarget.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.7.0;
+
+interface IAllowanceTarget {
+    function setSpenderWithTimelock(address _newSpender) external;
+
+    function completeSetSpender() external;
+
+    function executeCall(address payable _target, bytes calldata _callData) external returns (bytes memory resultData);
+
+    function teardown() external;
+}
