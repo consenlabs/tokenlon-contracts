ea63c5c79d0881b6574724b17e6edb8ac3f5a2b3
Charles Jhong <charlesjhongc@gmail.com>
2022-05-10 01:03:24 +0800

    add AMMQuoter and MarketMakerProxy

---
+++ contracts/MarketMakerProxy.sol
@@ -0,0 +1,111 @@
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "./interfaces/IWeth.sol";
+import "./utils/pmm/LibDecoder.sol";
+import "./Ownable.sol";
+
+interface IIMBTC {
+    function burn(uint256 amount, bytes calldata data) external;
+}
+
+interface IWBTC {
+    function burn(uint256 value) external;
+}
+
+contract MarketMakerProxy is Ownable, LibDecoder {
+    using SafeERC20 for IERC20;
+
+    string public constant version = "5.0.0";
+    uint256 private constant MAX_UINT = 2**256 - 1;
+    address public SIGNER;
+    address public operator;
+
+    // auto withdraw weth to eth
+    address public WETH_ADDR;
+    address public withdrawer;
+    mapping(address => bool) public isWithdrawWhitelist;
+
+    modifier onlyWithdrawer() {
+        require(msg.sender == withdrawer, "MarketMakerProxy: only contract withdrawer");
+        _;
+    }
+
+    modifier onlyOperator() {
+        require(msg.sender == operator, "MarketMakerProxy: only contract operator");
+        _;
+    }
+
+    constructor() Ownable(msg.sender) {
+        operator = msg.sender;
+    }
+
+    receive() external payable {}
+
+    // Manage
+    function setSigner(address _signer) public onlyOperator {
+        SIGNER = _signer;
+    }
+
+    function setConfig(address _weth) public onlyOperator {
+        WETH_ADDR = _weth;
+    }
+
+    function setWithdrawer(address _withdrawer) public onlyOperator {
+        withdrawer = _withdrawer;
+    }
+
+    function setOperator(address _newOperator) public onlyOwner {
+        operator = _newOperator;
+    }
+
+    function setAllowance(address[] memory token_addrs, address spender) public onlyOperator {
+        for (uint256 i = 0; i < token_addrs.length; i++) {
+            address token = token_addrs[i];
+            IERC20(token).safeApprove(spender, MAX_UINT);
+        }
+    }
+
+    function closeAllowance(address[] memory token_addrs, address spender) public onlyOperator {
+        for (uint256 i = 0; i < token_addrs.length; i++) {
+            address token = token_addrs[i];
+            IERC20(token).safeApprove(spender, 0);
+        }
+    }
+
+    function registerWithdrawWhitelist(address _addr, bool _add) public onlyOperator {
+        isWithdrawWhitelist[_addr] = _add;
+    }
+
+    function withdraw(
+        address token,
+        address payable to,
+        uint256 amount
+    ) public onlyWithdrawer {
+        require(isWithdrawWhitelist[to], "MarketMakerProxy: not in withdraw whitelist");
+        if (token == WETH_ADDR) {
+            IWETH(WETH_ADDR).withdraw(amount);
+            to.transfer(amount);
+        } else {
+            IERC20(token).safeTransfer(to, amount);
+        }
+    }
+
+    function withdrawETH(address payable to, uint256 amount) public onlyWithdrawer {
+        require(isWithdrawWhitelist[to], "MarketMakerProxy: not in withdraw whitelist");
+        to.transfer(amount);
+    }
+
+    function isValidSignature(bytes32 orderHash, bytes memory signature) public view returns (bytes32) {
+        require(SIGNER == _ecrecoverAddress(orderHash, signature), "MarketMakerProxy: invalid signature");
+        return keccak256("isValidWalletSignature(bytes32,address,bytes)");
+    }
+
+    function _ecrecoverAddress(bytes32 orderHash, bytes memory signature) internal pure returns (address) {
+        (uint8 v, bytes32 r, bytes32 s) = decodeMmSignature(signature);
+        return ECDSA.recover(ECDSA.toEthSignedMessageHash(orderHash), v, r, s);
+    }
+}
---
+++ contracts/interfaces/IBalancerV2Vault.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.7.0;
+pragma abicoder v2;
+
+/// @dev Minimal Balancer V2 Vault interface
+///      for documentation refer to https://github.com/balancer-labs/balancer-core-v2/blob/master/contracts/vault/interfaces/IVault.sol
+interface IBalancerV2Vault {
+    enum SwapKind {
+        GIVEN_IN,
+        GIVEN_OUT
+    }
+
+    struct BatchSwapStep {
+        bytes32 poolId;
+        uint256 assetInIndex;
+        uint256 assetOutIndex;
+        uint256 amount;
+        bytes userData;
+    }
+
+    struct FundManagement {
+        address sender;
+        bool fromInternalBalance;
+        address payable recipient;
+        bool toInternalBalance;
+    }
+
+    function queryBatchSwap(
+        SwapKind kind,
+        BatchSwapStep[] memory swaps,
+        address[] memory assets,
+        FundManagement memory funds
+    ) external returns (int256[] memory assetDeltas);
+
+    function batchSwap(
+        SwapKind kind,
+        BatchSwapStep[] memory swaps,
+        address[] memory assets,
+        FundManagement memory funds,
+        int256[] memory limits,
+        uint256 deadline
+    ) external payable returns (int256[] memory);
+}
