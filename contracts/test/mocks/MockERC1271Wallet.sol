c22a6e88449f316bb5691991e7ec3109be5cd283
NIC619 <twedusuck@gmail.com>
2022-07-13 15:08:47 +0800

    Verify signature in MockERC1271Wallet

--- contracts/test/mocks/MockERC1271Wallet.sol
+++ contracts/test/mocks/MockERC1271Wallet.sol
@@ -2,17 +2,20 @@ pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "contracts/utils/LibBytes.sol";
 import "../../interfaces/ISetAllowance.sol";
 import "../../interfaces/IERC1271Wallet.sol";
 
 contract MockERC1271Wallet is ISetAllowance, IERC1271Wallet {
     using SafeERC20 for IERC20;
+    using LibBytes for bytes;
+
     // bytes4(keccak256("isValidSignature(bytes,bytes)"))
     bytes4 internal constant ERC1271_MAGICVALUE = 0x20c13b0b;
-
     // bytes4(keccak256("isValidSignature(bytes32,bytes)"))
     bytes4 internal constant ERC1271_MAGICVALUE_BYTES32 = 0x1626ba7e;
     uint256 private constant MAX_UINT = 2**256 - 1;
+
     address public operator;
 
     modifier onlyOperator() {
@@ -37,10 +40,19 @@ contract MockERC1271Wallet is ISetAllowance, IERC1271Wallet {
     }
 
     function isValidSignature(bytes calldata _data, bytes calldata _signature) external view override returns (bytes4 magicValue) {
+        require(operator == _ecrecover(keccak256(_data), _signature), "MockERC1271Wallet: invalid signature");
         return ERC1271_MAGICVALUE;
     }
 
     function isValidSignature(bytes32 _hash, bytes calldata _signature) external view override returns (bytes4 magicValue) {
+        require(operator == _ecrecover(_hash, _signature), "MockERC1271Wallet: invalid signature");
         return ERC1271_MAGICVALUE_BYTES32;
+    }
+
+    function _ecrecover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+        uint8 v = uint8(signature[64]);
+        bytes32 r = signature.readBytes32(0);
+        bytes32 s = signature.readBytes32(32);
+        return ecrecover(hash, v, r, s);
     }
 }
