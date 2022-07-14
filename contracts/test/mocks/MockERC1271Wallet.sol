ed03041ed2aee6d365aeeb0ef8da6690dd3a2481
NIC619 <twedusuck@gmail.com>
2022-07-14 16:08:22 +0800

    Use OZ ECDSA recover

--- contracts/test/mocks/MockERC1271Wallet.sol
+++ contracts/test/mocks/MockERC1271Wallet.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.7.6;
 
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "contracts/utils/LibBytes.sol";
@@ -49,10 +50,10 @@ contract MockERC1271Wallet is ISetAllowance, IERC1271Wallet {
         return ERC1271_MAGICVALUE_BYTES32;
     }
 
-    function _ecrecover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+    function _ecrecover(bytes32 _hash, bytes memory signature) internal pure returns (address) {
         uint8 v = uint8(signature[64]);
         bytes32 r = signature.readBytes32(0);
         bytes32 s = signature.readBytes32(32);
-        return ecrecover(hash, v, r, s);
+        return ECDSA.recover(_hash, v, r, s);
     }
 }
