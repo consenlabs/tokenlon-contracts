b5c1bc093d83a27e1f3fea2cfd78617285d0f540
Eric Chang <hbea6311@gmail.com>
2022-07-26 16:55:12 +0800

    add test for withaw() and transfer()

--- contracts/test/veLON.t.sol
+++ contracts/test/veLON.t.sol
@@ -9,6 +9,7 @@ import "contracts-test/utils/BalanceSnapshot.sol";
 
 contract veLONTest is Test {
     using BalanceSnapshot for BalanceSnapshot.Snapshot;
+    using SafeMath for uint256;
 
     uint256 userPrivateKey = uint256(1);
     uint256 otherPricateKey = uint256(2);
@@ -19,8 +20,11 @@ contract veLONTest is Test {
     Lon lon = new Lon(address(this), address(this));
     veLON veLon;
 
-    uint256 DEFAULT_STAKE_AMOUNT = 1e18;
-    uint256 MAX_LOCK_TIME = 365 days;
+    uint256 constant DEFAULT_STAKE_AMOUNT = 1e18;
+    uint256 constant MAX_LOCK_TIME = 365 days;
+    uint256 public constant PENALTY_RATE_PRECISION = 10000;
+
+    uint256 public earlyWithdrawPenaltyRate = 3000;
 
     //record the balnce of Lon and VeLon in VM
     BalanceSnapshot.Snapshot stakerLon;
@@ -108,18 +112,19 @@ contract veLONTest is Test {
     function testCreateLock() public {
         _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
     }
+
     function testFuzzCreateLock(uint256 _amount) public {
         vm.prank(user);
-        vm.assume(_amount<= 100 * 1e18);
-        vm.assume(_amount>0);
+        vm.assume(_amount <= 100 * 1e18);
+        vm.assume(_amount > 0);
         veLon.createLock(_amount, MAX_LOCK_TIME);
     }
-    function testCannotCreateLockWithZero()public {
+
+    function testCannotCreateLockWithZero() public {
         vm.prank(user);
         vm.expectRevert("Zero lock amount");
         veLon.createLock(0, MAX_LOCK_TIME);
     }
-    
 
     function testDepositFor() public {
         uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
@@ -140,26 +145,26 @@ contract veLONTest is Test {
     function testFuzzDepositFor(uint256 _amount) public {
         uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
         vm.prank(user);
-        vm.assume(_amount<= 100 * 1e18);
-        vm.assume(_amount>0);
+        vm.assume(_amount <= 100 * 1e18);
+        vm.assume(_amount > 0);
         veLon.depositFor(tokenId, _amount);
     }
+
     function testExtendLock() public {
-        
         uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, 1 weeks);
         uint256 lockEndBefore = veLon.unlockTime(tokenId);
-        
+
         vm.prank(user);
         veLon.extendLock(tokenId, 2 weeks);
         uint256 lockEndAfter = veLon.unlockTime(tokenId);
 
-        require((lockEndAfter-lockEndBefore) == 1 weeks, "wrong time extended");
+        require((lockEndAfter - lockEndBefore) == 1 weeks, "wrong time extended");
     }
 
     function testCannoExtendLockExpired() public {
         uint256 lockTime = 1 weeks;
         uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, lockTime);
-        
+
         //pretend 1 week has passed and the lock expired
         vm.warp(block.timestamp + 1 weeks);
         vm.prank(user);
@@ -169,31 +174,70 @@ contract veLONTest is Test {
 
     function testMerge() public {
         uint256 _fromTokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
-        
+
         uint256 _toTokenId = _stakeAndValidate(other, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
         vm.prank(other);
-        veLon.approve(address(user),_toTokenId);
+        veLon.approve(address(user), _toTokenId);
 
         vm.prank(user);
         veLon.merge(_fromTokenId, _toTokenId);
-        
+
         //check whether fromToken has burned
         vm.expectRevert("ERC721: owner query for nonexistent token");
         veLon.ownerOf(_fromTokenId);
     }
-
 
     /*********************************
      *         Test: Withdraw        *
      *********************************/
 
     function testWithdraw() public {
-        uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, 2 weeks);
-        
+        uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, 1 weeks);
+
+        stakerLon = BalanceSnapshot.take(user, address(lon));
+        lockedLon = BalanceSnapshot.take(address(veLon), address(lon));
+
         //pretend 1 week has passed and the lock expired
         vm.prank(user);
-        //vm.warp(block.timestamp + 2 weeks);
+        vm.warp(block.timestamp + 1 weeks);
         veLon.withdraw(tokenId);
+        stakerLon.assertChange(int256(DEFAULT_STAKE_AMOUNT));
+        lockedLon.assertChange(int256(-(DEFAULT_STAKE_AMOUNT)));
     }
 
+    function testEarlyWithdrawWithPanalty() public {
+        uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, 2 weeks);
+
+        stakerLon = BalanceSnapshot.take(user, address(lon));
+        lockedLon = BalanceSnapshot.take(address(veLon), address(lon));
+
+        //pretend 1 week has passed and the lock not expired
+        vm.warp(block.timestamp + 1 weeks);
+        vm.prank(user);
+        veLon.withdraw(tokenId);
+        uint256 balanceChange = DEFAULT_STAKE_AMOUNT.mul(earlyWithdrawPenaltyRate).div(PENALTY_RATE_PRECISION);
+        stakerLon.assertChange(int256(DEFAULT_STAKE_AMOUNT.sub(balanceChange)));
+    }
+
+    /*********************************
+     *         Test: Transfer        *
+     *********************************/
+    function testTransferByOwner() public {
+        uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
+        vm.startPrank(user);
+        veLon.approve(other, tokenId);
+        veLon.transferFrom(user, other, tokenId);
+        vm.stopPrank();
+        assertEq(veLon.ownerOf(tokenId), other);
+    }
+
+    function testTransferByOther() public {
+        uint256 tokenId = _stakeAndValidate(user, DEFAULT_STAKE_AMOUNT, MAX_LOCK_TIME);
+        vm.prank(user);
+        veLon.approve(other, tokenId);
+        vm.prank(other);
+        veLon.transferFrom(user, other, tokenId);
+        vm.stopPrank();
+        assertEq(veLon.ownerOf(tokenId), other);
+    }
 }
