68f2cbcdf7daba02243d6c15817d47cb2be30129
Eric Chang <hbea6311@gmail.com>
2022-07-29 16:37:55 +0800

    improve the style of code comments

--- contracts/test/veLON/Withdraw.t.sol
+++ contracts/test/veLON/Withdraw.t.sol
@@ -12,14 +12,14 @@ contract TestVeLONWithfraw is TestVeLON {
         stakerLon = BalanceSnapshot.take(user, address(lon));
         lockedLon = BalanceSnapshot.take(address(veLon), address(lon));
 
-        //pretend 1 week has passed and the lock expired
+        // pretend 1 week has passed and the lock expired
         vm.prank(user);
         vm.warp(block.timestamp + 1 weeks);
         veLon.withdraw(tokenId);
         stakerLon.assertChange(int256(DEFAULT_STAKE_AMOUNT));
         lockedLon.assertChange(int256(-(DEFAULT_STAKE_AMOUNT)));
 
-        //check whether token has burned after withdraw
+        // check whether token has burned after withdraw
         vm.expectRevert("ERC721: owner query for nonexistent token");
         veLon.ownerOf(tokenId);
     }
@@ -30,14 +30,14 @@ contract TestVeLONWithfraw is TestVeLON {
         stakerLon = BalanceSnapshot.take(user, address(lon));
         lockedLon = BalanceSnapshot.take(address(veLon), address(lon));
 
-        //pretend 1 week has passed and the lock not expired
+        // pretend 1 week has passed and the lock not expired
         vm.warp(block.timestamp + 1 weeks);
         vm.prank(user);
         veLon.withdrawEarly(tokenId);
         uint256 balanceChange = DEFAULT_STAKE_AMOUNT.mul(earlyWithdrawPenaltyRate).div(PENALTY_RATE_PRECISION);
         stakerLon.assertChange(int256(DEFAULT_STAKE_AMOUNT.sub(balanceChange)));
 
-        //check whether token has burned after withdraw
+        // check whether token has burned after withdraw
         vm.expectRevert("ERC721: owner query for nonexistent token");
         veLon.ownerOf(tokenId);
     }
