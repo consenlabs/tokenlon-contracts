ada99f47df3140f4c6374157baf8a6550cd8ce99
Charles Jhong <charlesjhongc@gmail.com>
2022-08-15 16:27:50 +0800

    refactor abicoder pragma

--- contracts/MerkleRedeem.sol
+++ contracts/MerkleRedeem.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
-pragma experimental ABIEncoderV2;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
