a3e78f5a02c49a39d3e07d2933a7751b454d3244
Charles Jhong <charlesjhongc@gmail.com>
2022-08-24 14:46:03 +0800

    Reanme StakingRewards

--- contracts/interfaces/ILPStakingRewards.sol
+++ contracts/interfaces/ILPStakingRewards.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0;
 
-interface IStakingRewards {
+interface ILPStakingRewards {
     // Views
     function lastTimeRewardApplicable() external view returns (uint256);
 
