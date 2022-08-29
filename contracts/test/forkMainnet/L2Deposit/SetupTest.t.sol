4488a42bb225bb6c23a01e84caa17bbb58ccc668
Charles Jhong <charlesjhongc@gmail.com>
2022-08-29 16:51:15 +0800

    add test for L2Deposit

---
+++ contracts/test/forkMainnet/L2Deposit/SetupTest.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
+
+import "contracts-test/forkMainnet/L2Deposit/Setup.t.sol";
+
+contract TestL2DepositSetup is TestL2Deposit {
+    function testSetupL2Deposit() public {
+        assertEq(l2Deposit.owner(), address(this));
+        assertEq(l2Deposit.userProxy(), address(userProxy));
+        assertEq(address(l2Deposit.spender()), address(spender));
+        assertEq(address(l2Deposit.permStorage()), address(permanentStorage));
+        assertEq(address(l2Deposit.arbitrumL1GatewayRouter()), address(arbitrumL1GatewayRouter));
+        assertEq(address(l2Deposit.arbitrumL1Inbox()), address(arbitrumL1Inbox));
+        assertEq(address(l2Deposit.arbitrumL2RefundCollector()), address(arbitrumL2RefundCollector));
+        assertEq(address(l2Deposit.optimismL1StandardBridge()), address(optimismL1StandardBridge));
+    }
+}
