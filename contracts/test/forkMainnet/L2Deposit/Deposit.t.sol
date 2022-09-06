47106c565ecc2aec174a560bc8e56f3406f77be7
Charles Jhong <charlesjhongc@gmail.com>
2022-09-06 17:39:09 +0800

    fix typo

--- contracts/test/forkMainnet/L2Deposit/Deposit.t.sol
+++ contracts/test/forkMainnet/L2Deposit/Deposit.t.sol
@@ -106,7 +106,7 @@ contract TestL2DepositTopUp is TestL2Deposit {
         bytes memory sig = _signDeposit(userPrivateKey, DEFAULT_DEPOSIT);
         bytes memory payload = abi.encodeWithSelector(L2Deposit.deposit.selector, IL2Deposit.DepositParams(DEFAULT_DEPOSIT, sig));
 
-        // conpose inbox revert data with defined error
+        // compose inbox revert data with defined error
         uint256 l2Callvalue = 0; // l2 call value 0 by default
         uint256 l1Cost = arbMaxSubmissionCost + l2Callvalue + (arbMaxGas * arbGasPriceBid);
         // Sig : error InsufficientValue(uint256 expected, uint256 actual)
