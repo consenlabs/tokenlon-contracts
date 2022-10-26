e7a24e70106164a5fe93733d7a22f2e1e30a960c
Charles Jhong <charlesjhongc@gmail.com>
2022-10-26 08:44:49 +0800

    refactor DepositETH params

--- contracts/interfaces/IStrategyBase.sol
+++ contracts/interfaces/IStrategyBase.sol
@@ -5,7 +5,7 @@ interface IStrategyBase {
     event UpgradeSpender(address newSpender);
     event AllowTransfer(address indexed spender, address token);
     event DisallowTransfer(address indexed spender, address token);
-    event DepositETH(uint256 ethBalance);
+    event DepositETH(uint256 amount);
 
     function upgradeSpender(address _newSpender) external;
 
