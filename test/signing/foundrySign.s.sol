67327678de6a08bd208576c8d75ce8c607373b46
NIC619 <twedusuck@gmail.com>
2023-01-10 11:01:26 +0800

    Add Foundry sign script

---
+++ test/signing/foundrySign.s.sol
@@ -0,0 +1,61 @@
+pragma solidity 0.7.6;
+
+import "forge-std/Script.sol";
+
+import "contracts/AMMWrapper.sol";
+import "contracts/utils/AMMLibEIP712.sol";
+import { getEIP712Hash } from "test/utils/Sig.sol";
+
+function computeEIP712DomainSeparator(address verifyingContract) returns (bytes32) {
+    uint256 CHAIN_ID = 1;
+    bytes32 EIP712_DOMAIN_SEPARATOR = keccak256(
+        abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes("Tokenlon")),
+            keccak256(bytes("v5")),
+            CHAIN_ID,
+            verifyingContract
+        )
+    );
+    return EIP712_DOMAIN_SEPARATOR;
+}
+
+contract SigningScript is Script {
+    function run() external {
+        checkAMMSig();
+    }
+
+    function checkAMMSig() internal {
+        string memory ammWrapperPayloadJson = vm.readFile("test/signing/payload/ammWrapper.json");
+
+        AMMLibEIP712.Order memory order = AMMLibEIP712.Order(
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "makerAddr"), (address)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "takerAssetAddr"), (address)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "makerAssetAddr"), (address)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "takerAssetAmount"), (uint256)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "makerAssetAmount"), (uint256)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "userAddr"), (address)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "receiverAddr"), (address)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "salt"), (uint256)),
+            abi.decode(vm.parseJson(ammWrapperPayloadJson, "deadline"), (uint256))
+        );
+
+        address ammWrapperAddr = abi.decode(vm.parseJson(ammWrapperPayloadJson, "AMMWrapper"), (address));
+        uint256 userPrivateKey = abi.decode(vm.parseJson(ammWrapperPayloadJson, "signingKey"), (uint256));
+        bytes memory sig = _signAMMTrade(ammWrapperAddr, userPrivateKey, order);
+
+        bytes memory expectedSig = abi.decode(vm.parseJson(ammWrapperPayloadJson, "expectedSig"), (bytes));
+        require(keccak256(sig) == keccak256(expectedSig), "Not expected AMM order sig");
+    }
+
+    function _signAMMTrade(
+        address ammWrapperAddr,
+        uint256 privateKey,
+        AMMLibEIP712.Order memory order
+    ) internal returns (bytes memory sig) {
+        bytes32 orderHash = AMMLibEIP712._getOrderHash(order);
+        bytes32 EIP712SignDigest = getEIP712Hash(computeEIP712DomainSeparator(ammWrapperAddr), orderHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, EIP712SignDigest);
+        sig = abi.encodePacked(r, s, v, bytes32(0), uint8(2));
+    }
+}
