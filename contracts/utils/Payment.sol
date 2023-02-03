60a934763bed8f4d05efa1f8c95639bfa9627f51
pilagod <pilagooood@gmail.com>
2023-02-03 15:21:27 +0800

    add payment from token

--- contracts/utils/Payment.sol
+++ contracts/utils/Payment.sol
@@ -1,6 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-library Payment {
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
+library Payment {
+    using SafeERC20 for IERC20;
+
+    enum Type {
+        Token
+    }
+
+    function fulfill(
+        address payer,
+        address token,
+        uint256 amount,
+        bytes memory data
+    ) internal {
+        (Type t, bytes memory data) = abi.decode(data, (Type, bytes));
+        if (t == Type.Token) {
+            return transferFromToken(payer, token, amount, data);
+        }
+    }
+
+    function transferFromToken(
+        address payer,
+        address token,
+        uint256 amount,
+        bytes memory data
+    ) private {
+        // TODO: Parse data for permit
+        IERC20(token).safeTransferFrom(payer, address(this), amount);
+    }
 }
--- test/Payment.t.sol
+++ test/Payment.t.sol
@@ -2,14 +2,36 @@
 pragma solidity 0.7.6;
 
 import { Test } from "forge-std/Test.sol";
+import { Payment } from "contracts/utils/Payment.sol";
+import { MockERC20 } from "test/mocks/MockERC20.sol";
 
 contract TestPayment is Test {
+    uint256 userPrivateKey = uint256(1);
+    address user = vm.addr(userPrivateKey);
+
+    MockERC20 token = new MockERC20("Token", "TKN", 18);
+
     function setUp() public virtual {
+        token.mint(user, 10000 * 1e18);
+
         vm.label(address(this), "TestingContract");
+        vm.label(address(token), "TKN");
     }
 
     function testCanary() public {
         assertTrue(true);
     }
-}
 
+    function testPayByDirectlyApproval() public {
+        uint256 amount = 100 * 1e18;
+
+        vm.prank(user);
+        token.approve(address(this), amount);
+
+        bytes memory data = abi.encode(Payment.Type.Token, bytes(""));
+        Payment.fulfill(user, address(token), amount, data);
+
+        uint256 balance = token.balanceOf(address(this));
+        assertEq(balance, amount);
+    }
+}
