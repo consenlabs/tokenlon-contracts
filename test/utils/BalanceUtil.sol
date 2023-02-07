56af2af9be45dd2ebdbb03cea14da2c5036afa71
NIC619 <twedusuck@gmail.com>
2023-02-07 12:06:05 +0800

    setERC20Balance update totalSupply in local, not in fork

--- test/utils/BalanceUtil.sol
+++ test/utils/BalanceUtil.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
+import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "test/utils/Addresses.sol";
 
-contract BalanceUtil is Addresses {
+contract BalanceUtil is Test {
     using stdStorage for StdStorage;
 
     function setERC20Balance(
@@ -13,13 +13,6 @@ contract BalanceUtil is Addresses {
         uint256 amount
     ) internal {
         uint256 decimals = uint256(ERC20(tokenAddr).decimals());
-        // Skip setting WETH's totalSupply because WETH does not store total supply in storage
-        bool updateTotalSupply = tokenAddr == WETH_ADDRESS ? false : true;
-        deal(
-            tokenAddr,
-            userAddr,
-            amount * (10**decimals),
-            updateTotalSupply // also update totalSupply
-        );
+        deal(tokenAddr, userAddr, amount * (10**decimals), isFork() ? false : true);
     }
 }
