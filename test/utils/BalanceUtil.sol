6e5399253f38c88752496007dedd1ffbedc99181
NIC619 <twedusuck@gmail.com>
2023-02-08 10:47:59 +0800

    setERC20Balance use try/catch to update totalSupply

--- test/utils/BalanceUtil.sol
+++ test/utils/BalanceUtil.sol
@@ -7,12 +7,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract BalanceUtil is Test {
     using stdStorage for StdStorage;
 
+    function externalDeal(
+        address tokenAddr,
+        address userAddr,
+        uint256 amountInWei,
+        bool updateTotalSupply
+    ) public {
+        deal(tokenAddr, userAddr, amountInWei, updateTotalSupply);
+    }
+
     function setERC20Balance(
         address tokenAddr,
         address userAddr,
         uint256 amount
     ) internal {
         uint256 decimals = uint256(ERC20(tokenAddr).decimals());
-        deal(tokenAddr, userAddr, amount * (10**decimals), isFork() ? false : true);
+        uint256 amountInWei = amount * (10**decimals);
+        // First try to update `totalSupply` together, but this would fail with WETH because WETH does not store `totalSupply` in storage
+        try this.externalDeal(tokenAddr, userAddr, amountInWei, true) {} catch {
+            // If it fails, try again without update `totalSupply`
+            deal(tokenAddr, userAddr, amountInWei, false);
+        }
     }
 }
