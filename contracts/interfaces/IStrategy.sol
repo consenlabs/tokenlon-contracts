d193d5c83287e4322c5f70b86b9347a8be599607
Charles Jhong <charlesjhongc@gmail.com>
2023-02-09 09:38:13 +0800

    add v6 IStrategy

---
+++ contracts/interfaces/IStrategy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+/// @title IStrategy Interface
+/// @author imToken Labs
+interface IStrategy {
+    function executeStrategy(
+        address inputToken,
+        address outputToken,
+        uint256 inputAmount,
+        bytes calldata data
+    ) external;
+}
