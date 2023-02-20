068d9bc4cf6d78be2ebfe879c25ec7c087a4962e
doublespending <outofgas@qq.com>
2023-02-20 14:30:21 +0800

    refactor amm strategy

--- contracts/AMMStrategy.sol
+++ contracts/AMMStrategy.sol
@@ -3,71 +3,78 @@ pragma solidity 0.8.17;
 // interface, abstract, contract 0.8.17
 pragma abicoder v2;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./interfaces/IStrategy.sol";
+import "./interfaces/IAMMStrategy.sol";
 import "./interfaces/IUniswapRouterV2.sol";
 
-contract AMMStrategy is IStrategy, ReentrancyGuard, Ownable {
+contract AMMStrategy is IAMMStrategy, ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
-
-    struct Operation {
-        address dest;
-        bytes data;
-    }
-
-    /// @notice Emitted when entry point address is updated
-    /// @param newEntryPoint The address of the new entry point
-    event SetEntryPoint(address newEntryPoint);
-
-    /// @notice Emitted after swap with AMM
-    /// @param takerAssetAddr The taker assest used to swap
-    /// @param takerAssetAmount The swap amount of taker asset
-    /// @param makerAddr The address of maker
-    /// @param makerAssetAddr The maker assest used to swap
-    /// @param makerAssetAmount The swap amount of maker asset
-    event Swapped(address takerAssetAddr, uint256 takerAssetAmount, address[] makerAddr, address makerAssetAddr, uint256 makerAssetAmount);
+    using SafeMath for uint256;
 
     address public entryPoint;
     mapping(address => bool) public ammMapping;
 
-    constructor(address _entryPoint, address[] memory amms) {
+    /************************************************************
+     *              Constructor and init functions               *
+     *************************************************************/
+
+    constructor(address _entryPoint, address[] memory _ammAddrs) {
         entryPoint = _entryPoint;
-        for (uint256 i = 0; i < amms.length; ++i) {
-            ammMapping[amms[i]] = true;
-            // should emit
+        for (uint256 i = 0; i < _ammAddrs.length; ++i) {
+            ammMapping[_ammAddrs[i]] = true;
+            emit SetAMM(_ammAddrs[i], true);
         }
     }
 
+    /************************************************************
+     *                 Internal function modifier                *
+     *************************************************************/
     modifier onlyEntryPoint() {
         require(msg.sender == entryPoint, "only entry point");
         _;
     }
 
-    function approveTakerAsset(
-        address takerAssetAddr,
-        address makerAddr,
-        uint256 takerAssetAmount
-    ) external onlyOwner {
-        IERC20(takerAssetAddr).safeApprove(makerAddr, takerAssetAmount);
-    }
-
-    function approveAMMs(address[] calldata amms, bool[] calldata enables) external onlyOwner {
-        for (uint256 i = 0; i < amms.length; ++i) {
-            ammMapping[amms[i]] = enables[i];
-            // should emit
-        }
-    }
-
+    /************************************************************
+     *           Management functions for Owner               *
+     *************************************************************/
+    /// @notice Only owner can call
+    /// @param _newEntryPoint The address allowed to call `executeStrategy`
     function setEntryPoint(address _newEntryPoint) external onlyOwner {
         entryPoint = _newEntryPoint;
         emit SetEntryPoint(_newEntryPoint);
     }
 
-    // only work for erc-20
+    /// @notice Only owner can call
+    /// @param _ammAddrs The amm addresses allowed to use in `executeStrategy` if according `enable` equals `true`
+    /// @param _enables The status of accouring amm addresses
+    function setAMMs(address[] calldata _ammAddrs, bool[] calldata _enables) external onlyOwner {
+        for (uint256 i = 0; i < _ammAddrs.length; ++i) {
+            ammMapping[_ammAddrs[i]] = _enables[i];
+            emit SetAMM(_ammAddrs[i], true);
+        }
+    }
+
+    /// @notice Only owner can call
+    /// @param _assetAddr The asset address
+    /// @param _ammAddr The approved amm address
+    /// @param _assetAmount The approved asset amount
+    function approveAsset(
+        address _assetAddr,
+        address _ammAddr,
+        uint256 _assetAmount
+    ) external onlyOwner {
+        IERC20(_assetAddr).safeApprove(_ammAddr, _assetAmount);
+    }
+
+    /************************************************************
+     *                   External functions                      *
+     *************************************************************/
+    /// @inheritdoc IStrategy
     function executeStrategy(
         address srcToken,
         uint256 inputAmount,
@@ -89,15 +96,19 @@ contract AMMStrategy is IStrategy, ReentrancyGuard, Ownable {
             IERC20(targetToken).safeTransfer(entryPoint, receivedAmount);
         }
         uint256 balanceAfter = IERC20(targetToken).balanceOf(entryPoint);
-        emit Swapped(srcToken, inputAmount, opDests, targetToken, balanceAfter - balanceBefore);
+        emit Swapped(srcToken, inputAmount, opDests, targetToken, balanceAfter.sub(balanceBefore));
     }
 
+    /**
+     * @dev internal function of `executeStrategy`.
+     * Used to execute arbitrary calls.
+     */
     function _call(
-        address target,
-        uint256 value,
-        bytes memory data
+        address _dest,
+        uint256 _value,
+        bytes memory _data
     ) internal {
-        (bool success, bytes memory result) = target.call{ value: value }(data);
+        (bool success, bytes memory result) = _dest.call{ value: _value }(_data);
         if (!success) {
             assembly {
                 revert(add(result, 32), mload(result))
---
+++ contracts/interfaces/IAMMStrategy.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+pragma abicoder v2;
+
+import "./IStrategy.sol";
+
+/// @title IAMMWrapperWithPath Interface
+/// @author imToken Labs
+interface IAMMStrategy is IStrategy {
+    /// @notice Emitted when entry point address is updated
+    /// @param newEntryPoint The address of the new entry point
+    event SetEntryPoint(address newEntryPoint);
+
+    /// @notice Emitted when allowed amm is updated
+    /// @param ammAddr The address of the amm
+    /// @param enable The status of amm
+    event SetAMM(address ammAddr, bool enable);
+
+    /// @notice Emitted after swap with AMM
+    /// @param takerAssetAddr The taker assest used to swap
+    /// @param takerAssetAmount The swap amount of taker asset
+    /// @param makerAddr The address of maker
+    /// @param makerAssetAddr The maker assest used to swap
+    /// @param makerAssetAmount The swap amount of maker asset
+    event Swapped(address takerAssetAddr, uint256 takerAssetAmount, address[] makerAddr, address makerAssetAddr, uint256 makerAssetAmount);
+
+    /** @dev The encoded Operation list should be passed as `data` when calling `IStrategy.executeStrategy` */
+    struct Operation {
+        address dest;
+        bytes data;
+    }
+}
