b06c1e745f0b03f09f4ce11ea4373828809ae59b
doublespending <outofgas@qq.com>
2023-02-22 09:51:11 +0800

    add unit test for uniswap v3

--- test/forkMainnet/AMMStrategy/TradeWithSingleAMM.t.sol
+++ test/forkMainnet/AMMStrategy/TradeWithSingleAMM.t.sol
@@ -24,7 +24,36 @@ contract TestAMMStrategyTradeWithSingleAMM is TestAMMStrategy {
         entryPointTakerAsset.assertChange(-int256(entry.takerAssetAmount));
         ammStrategyTakerAsset.assertChange(int256(entry.takerAssetAmount));
 
-        (address srcToken, uint256 inputAmount, address targetToken, bytes memory data) = _genUniswapV2TradePayload(entry, true);
+        (address srcToken, uint256 inputAmount, address targetToken, bytes memory data, ) = _genUniswapV2TradePayload(entry);
+
+        entryPointTakerAsset = BalanceSnapshot.take(address(entryPoint), entry.takerAssetAddr);
+        ammStrategyTakerAsset = BalanceSnapshot.take(address(ammStrategy), entry.takerAssetAddr);
+        BalanceSnapshot.Snapshot memory entryPointMakerAsset = BalanceSnapshot.take(address(entryPoint), entry.makerAssetAddr);
+        BalanceSnapshot.Snapshot memory ammStrategyMakerAsset = BalanceSnapshot.take(address(ammStrategy), entry.makerAssetAddr);
+
+        vm.prank(entryPoint);
+        // ammStrategy swaps taker asset to maker asset and sends maker asset to entry point
+        ammStrategy.executeStrategy(srcToken, inputAmount, targetToken, data);
+        vm.stopPrank();
+        entryPointTakerAsset.assertChange(0);
+        ammStrategyTakerAsset.assertChange(-int256(entry.takerAssetAmount));
+        entryPointMakerAsset.assertChangeGt(0);
+        ammStrategyMakerAsset.assertChange(0);
+    }
+
+    function testTradeWithUniswapV3() public {
+        AMMStrategyEntry memory entry = DEFAULT_ENTRY;
+        entry.makerAddr = UNISWAP_V3_ADDRESS;
+        BalanceSnapshot.Snapshot memory entryPointTakerAsset = BalanceSnapshot.take(address(entryPoint), entry.takerAssetAddr);
+        BalanceSnapshot.Snapshot memory ammStrategyTakerAsset = BalanceSnapshot.take(address(ammStrategy), entry.takerAssetAddr);
+
+        // entryPoint sends taker asset to ammStrategy befor calling executeStrategy
+        _sendTakerAssetFromEntryPoint(entry.takerAssetAddr, entry.takerAssetAmount);
+
+        entryPointTakerAsset.assertChange(-int256(entry.takerAssetAmount));
+        ammStrategyTakerAsset.assertChange(int256(entry.takerAssetAmount));
+
+        (address srcToken, uint256 inputAmount, address targetToken, bytes memory data, ) = _genUniswapV3TradePayload(entry);
 
         entryPointTakerAsset = BalanceSnapshot.take(address(entryPoint), entry.takerAssetAddr);
         ammStrategyTakerAsset = BalanceSnapshot.take(address(ammStrategy), entry.takerAssetAddr);
