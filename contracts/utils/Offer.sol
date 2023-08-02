37af398066430531233c8bc765c039eaf8c21196
Charles Jhong <charlesjhongc@gmail.com>
2023-08-02 15:43:38 +0800

    fix RFQv2 typehash calculation

--- contracts/utils/Offer.sol
+++ contracts/utils/Offer.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.7.6;
 
 string constant OFFER_TYPESTRING = "Offer(address taker,address maker,address takerToken,uint256 takerTokenAmount,address makerToken,uint256 makerTokenAmount,uint256 expiry,uint256 salt)";
 
-bytes32 constant OFFER_DATA_TYPEHASH = 0x8db5bc1860cbde6bde04997e545735e15cdf6116ceb84d3ca908396a40da3e59;
-// keccak256(OFFER_TYPESTRING);
+bytes32 constant OFFER_DATA_TYPEHASH = keccak256(bytes(OFFER_TYPESTRING));
 
 struct Offer {
     address taker;
