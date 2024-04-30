74b0ae846b29bd9a0632ed66155479eee56d5071
david_tsai <108356037@nccu.edu.tw>
2024-04-30 15:18:45 +0800

    add gen RFQ test

---
+++ test/forkMainnet/GenRFQData.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+import { Tokens } from "test/utils/Tokens.sol";
+import { BalanceUtil } from "test/utils/BalanceUtil.sol";
+import { RFQOffer, getRFQOfferHash } from "contracts/libraries/RFQOffer.sol";
+import { RFQTx, getRFQTxHash } from "contracts/libraries/RFQTx.sol";
+import { Constant } from "contracts/libraries/Constant.sol";
+import { IGenericSwap } from "contracts/interfaces/IGenericSwap.sol";
+import { ISmartOrderStrategy } from "contracts/interfaces/ISmartOrderStrategy.sol";
+import { GenericSwapData, getGSDataHash } from "contracts/libraries/GenericSwapData.sol";
+import { IRFQ } from "contracts/interfaces/IRFQ.sol";
+import { SigHelper } from "test/utils/SigHelper.sol";
+
+contract GenRFQData is Test, Tokens, BalanceUtil, SigHelper {
+    address deployedGS = 0xa7e96Bf2735BD33750Bb504C3Cc63e3770668dd4;
+    address deployedRFQ = 0xC6e1074113a954340277aE6F309aF2AF6e259283;
+    address deployedSOR = 0x0E67fD506Db5C6199C5D2b2b54380DEB414E2431;
+
+    address taker = 0x40B38765696e3d5d8d9d834D8AaD4bB6e418E489;
+    uint256 makerKey = uint256(94530678);
+    address maker = vm.addr(makerKey);
+
+    function setUp() public {
+        BalanceUtil.setTokenBalanceAndApprove(taker, deployedGS, tokens, 1000000000000);
+        BalanceUtil.setTokenBalanceAndApprove(taker, deployedRFQ, tokens, 1000000000000);
+        BalanceUtil.setTokenBalanceAndApprove(maker, deployedRFQ, tokens, 1000000000000);
+    }
+
+    function test_gen_RFQ_data_called_from_GS() public {
+        address testTakerToken = vm.envAddress("TAKER_TOKEN");
+        address testMakerToken = vm.envAddress("MAKER_TOKEN");
+
+        RFQOffer memory rfqOffer = RFQOffer({
+            taker: deployedSOR,
+            takerToken: testTakerToken,
+            takerTokenAmount: 5000000,
+            maker: payable(maker),
+            makerToken: testMakerToken,
+            makerTokenAmount: 20000000000,
+            expiry: 2 ** 256 - 1,
+            feeFactor: 0,
+            flags: 86844066927987146567678238756515930889952488499230423029593188005934847229952,
+            salt: 55688
+        });
+        RFQTx memory rfqTx = RFQTx({ rfqOffer: rfqOffer, takerRequestAmount: 5000000, recipient: payable(deployedSOR) });
+        bytes memory makerSig = signRFQOffer(makerKey, rfqOffer, address(deployedRFQ));
+
+        ISmartOrderStrategy.Operation memory ops1 = ISmartOrderStrategy.Operation({
+            dest: deployedRFQ,
+            inputToken: testTakerToken,
+            inputRatio: 0,
+            dataOffset: 0,
+            value: 0,
+            data: abi.encodeCall(IRFQ.fillRFQ, (rfqTx, makerSig, hex"01", hex"01"))
+            // data: abi.encodeWithSelector(IRFQ.fillRFQ.selector, rfqTx, makerSig, hex"01", hex"01")
+        });
+        ISmartOrderStrategy.Operation[] memory operations = new ISmartOrderStrategy.Operation[](1);
+        operations[0] = ops1;
+
+        GenericSwapData memory defaultGSData = GenericSwapData({
+            maker: payable(address(deployedSOR)),
+            takerToken: testTakerToken,
+            takerTokenAmount: 5000000,
+            makerToken: Constant.ETH_ADDRESS,
+            makerTokenAmount: 20000000000, // to be filled later
+            minMakerTokenAmount: 0, // to be filled later
+            expiry: 2 ** 256 - 1,
+            salt: 5678,
+            recipient: payable(taker),
+            strategyData: abi.encode(operations)
+        });
+
+        vm.prank(taker, taker);
+        IGenericSwap(deployedGS).executeSwap(defaultGSData, hex"01");
+        bytes memory gsCalldata = abi.encodeWithSelector(IGenericSwap.executeSwap.selector, defaultGSData, hex"01");
+
+        console.log(taker);
+        console.log(rfqOffer.maker);
+        console.logBytes(gsCalldata);
+    }
+}
