19a33e74577d896da70bdddfd346d34d5c5c4a19
alex0207s <alexliao-dev@proton.me>
2024-05-28 15:43:58 +0800

    chore: remove unused contracts

--- contracts/interfaces/IMulticall.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-interface IMulticall {
-    event MulticallFailure(uint256 index, string reason);
-
-    function multicall(bytes[] calldata data, bool revertOnFail) external returns (bool[] memory successes, bytes[] memory results);
-}
